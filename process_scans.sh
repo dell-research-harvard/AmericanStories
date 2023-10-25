6816c7085f1d3b404fca4967a68b39e62c4ef5ee
bryanptom <bryanptom@gmail.com>
2023-10-25 09:31:31 -0400

    Fix process_scans for colab

--- process_scans.sh
+++ process_scans.sh
@@ -1,13 +1,13 @@
-python ./src/run_img2txt_yolo_pipeline.py --manifest_path /scans \
-									    --checkpoint_path_layout american_stories_models/layout_model_new.onnx \
-									    --checkpoint_path_line american_stores_models/line_model_new.onnx \
-									    --label_map_path_layout ./src/label_maps/label_map_layout.json \
-									    --label_map_path_line ./src/label_maps/label_map_line.json \
-								        --effocr_recognizer_dir american_stories_models/word_recognizer_new \
-									    --effocr_char_recognizer_dir american_stories_models/char_recognizer \
-								        --effocr_localizer_dir american_stories_models/localizer_model_new.onnx \
-								        --output_save_path ./outputs \
-									    --legibility-classifier american_stories_models/legibility_model_new.onnx \
+python ./AmericanStories/src/run_img2txt_yolo_pipeline.py --manifest_path ./scans \
+									    --checkpoint_path_layout ./american_stories_models/layout_model_new.onnx \
+									    --checkpoint_path_line ./american_stories_models/line_model_new.onnx \
+									    --label_map_path_layout ./AmericanStories/src/label_maps/label_map_layout.json \
+									    --label_map_path_line ./AmericanStories/src/label_maps/label_map_line.json \
+								        --effocr_recognizer_dir ./american_stories_models/word_recognizer_new \
+									    --effocr_char_recognizer_dir ./american_stories_models/char_recognizer \
+								        --effocr_localizer_dir ./american_stories_models/localizer_model_new.onnx \
+								        --output_save_path ./AmericanStories/outputs \
+									    --legibility-classifier ./american_stories_models/legibility_model_new.onnx \
 									    --localizer_model_backend yolov8 \
 									    --line_model_backend yolov8 \
 									    --layout_model_backend yolov8 \
